b484cdc61a76a4970793e18d91f0df017ce43a8a
Joshua Fraustro <36318163+jwfraustro@users.noreply.github.com>
2023-12-08 17:45:16 -0500

    updated missed utc timestamp references (#3)

--- tests/voresource/voresource_types_test.py
+++ tests/voresource/voresource_types_test.py
@@ -4,39 +4,39 @@ from unittest import TestCase
 from vo_models.xml.voresource.types import UTCTimestamp
 
 
-class TestVODatetimeModel(TestCase):
-    """Test VODatetime parsing"""
+class TestUTCTimestampModel(TestCase):
+    """Test UTCTimestamp parsing"""
 
-    def test_vodatetime_parse(self):
+    def test_utctimestamp_parse(self):
         """Test that datetimes are parsed and output in correct format"""
 
         # Test allowed string formats
-        good_vo_dt = "2023-03-15T18:27:18.758Z"
+        good_vo_utc = "2023-03-15T18:27:18.758Z"
 
         # 2023-03-15T18:27:18.758 (No timezone - Z UTC assumed)
-        vo_dt = UTCTimestamp.fromisoformat("2023-03-15T18:27:18.758")
-        self.assertIsInstance(vo_dt, UTCTimestamp)
-        self.assertEqual(vo_dt.isoformat(), good_vo_dt)
+        vo_utc = UTCTimestamp.fromisoformat("2023-03-15T18:27:18.758")
+        self.assertIsInstance(vo_utc, UTCTimestamp)
+        self.assertEqual(vo_utc.isoformat(), good_vo_utc)
 
         # 2023-03-15T18:27:18.758Z (Zulu UTC - T separator)
-        vo_dt = UTCTimestamp.fromisoformat("2023-03-15T18:27:18.758Z")
-        self.assertIsInstance(vo_dt, UTCTimestamp)
-        self.assertEqual(vo_dt.isoformat(), good_vo_dt)
+        vo_utc = UTCTimestamp.fromisoformat("2023-03-15T18:27:18.758Z")
+        self.assertIsInstance(vo_utc, UTCTimestamp)
+        self.assertEqual(vo_utc.isoformat(), good_vo_utc)
 
         # 2023-03-15 18:27:18.758Z (Zulu UTC - space separator)
-        vo_dt = UTCTimestamp.fromisoformat("2023-03-15 18:27:18.758Z")
-        self.assertIsInstance(vo_dt, UTCTimestamp)
-        self.assertEqual(vo_dt.isoformat(), good_vo_dt)
+        vo_utc = UTCTimestamp.fromisoformat("2023-03-15 18:27:18.758Z")
+        self.assertIsInstance(vo_utc, UTCTimestamp)
+        self.assertEqual(vo_utc.isoformat(), good_vo_utc)
 
         # 2023-03-15T18:27:18.758+00:00 (UTC w/ offset - T separator)
-        vo_dt = UTCTimestamp.fromisoformat("2023-03-15T18:27:18.758+00:00")
-        self.assertIsInstance(vo_dt, UTCTimestamp)
-        self.assertEqual(vo_dt.isoformat(), good_vo_dt)
+        vo_utc = UTCTimestamp.fromisoformat("2023-03-15T18:27:18.758+00:00")
+        self.assertIsInstance(vo_utc, UTCTimestamp)
+        self.assertEqual(vo_utc.isoformat(), good_vo_utc)
 
         # 2023-03-15 18:27:18.758+00:00 (UTC w/ offset - space separator)
-        vo_dt = UTCTimestamp.fromisoformat("2023-03-15 18:27:18.758+00:00")
-        self.assertIsInstance(vo_dt, UTCTimestamp)
-        self.assertEqual(vo_dt.isoformat(), good_vo_dt)
+        vo_utc = UTCTimestamp.fromisoformat("2023-03-15 18:27:18.758+00:00")
+        self.assertIsInstance(vo_utc, UTCTimestamp)
+        self.assertEqual(vo_utc.isoformat(), good_vo_utc)
 
         # Test that we reject non-UTC datetimes
         with self.assertRaises(ValueError):
