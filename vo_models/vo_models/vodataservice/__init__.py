cadcaef88eaf1e15d90e5b9027769c1c0a109e31
Joshua Fraustro <jfraustro@stsci.edu>
2023-12-13 20:22:27 -0500

    init

---
+++ vo_models/vo_models/vodataservice/__init__.py
@@ -0,0 +1,10 @@
+"""Module containing models and resources for IVOA VODataService objects."""
+from mast.vo_tap.services.vo_models.vodataservice.models import (
+    DataType,
+    FKColumn,
+    ForeignKey,
+    Table,
+    TableParam,
+    TableSchema,
+    TableSet,
+)
