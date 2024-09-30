0386b97a876f6fc306be6e4527651def8813b175
Joshua Fraustro <jfraustro@stsci.edu>
2024-09-30 10:33:28 -0400

    update imports

--- vo_models/vodataservice/__init__.py
+++ vo_models/vodataservice/__init__.py
@@ -1,8 +1,11 @@
 """Module containing models and resources for IVOA VODataService objects."""
 from vo_models.vodataservice.models import (
+    BaseParam,
     DataType,
     FKColumn,
     ForeignKey,
+    InputParam,
+    ParamHTTP,
     Table,
     TableParam,
     TableSchema,
